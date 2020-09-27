258ec47f126540993b6fb8871bfe1faac0473b47
Miguel Aquino <jcaquino0945@gmail.com>
2020-09-27 22:32:24 +0800

    charles with Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutUsComponent} from  './about-us/about-us.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+import {EventsComponent} from './events/events.component';
+import {SeriesComponent} from './series/series.component';
+import {TheTeamComponent} from './the-team/the-team.component';
+import {PagenotfoundComponent} from './pagenotfound/pagenotfound.component';
+const routes: Routes = [
+  {path:'homeComponent',component:HomeComponent},
+  {path:'aboutUsComponent',component:AboutUsComponent},
+  {path:'contactUsComponent',component:ContactUsComponent},
+  {path:'eventsComponent',component:EventsComponent},
+  {path:'seriesComponent',component:SeriesComponent},
+  {path:'theTeamComponent',component:TheTeamComponent},
+  {path:'',redirectTo:'/homeComponent',pathMatch:'full'}, //if no path, redirects to home
+  {path:'**',component:PagenotfoundComponent} //pagenotfound page
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { TheTeamComponent } from './the-team/the-team.component';
+import { SeriesComponent } from './series/series.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { EventsComponent } from './events/events.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    TheTeamComponent,
+    SeriesComponent,
+    ContactUsComponent,
+    EventsComponent,
+    PagenotfoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
